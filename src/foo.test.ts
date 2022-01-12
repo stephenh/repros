d4debe55c0c32de661e52f3a3a47d867df75fdbe
Stephen Haberman <stephen.haberman@gmail.com>
2022-01-12 12:27:03 -0600

    Attempt repro.

---
+++ src/foo.test.ts
@@ -0,0 +1,22 @@
+
+describe("foo", () => {
+  it("asserts something", async () => {
+    await 1;
+    expect(1).toEqual(2);
+  })
+
+  it("asserts something", async () => {
+    await 1;
+    expect(1).toEqual(2);
+  })
+
+  it("asserts something", async () => {
+    await 1;
+    expect(1).toEqual(2);
+  })
+
+  it("asserts something", async () => {
+    await 1;
+    expect(1).toEqual(2);
+  })
+})
